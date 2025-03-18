ff70c2f442f6bf3d510b474d93fdac52f2e2743b
VF <vladyslav.filippov@jatapp.com>
2025-03-18 14:39:54 +0200

    Initial Commit

---
+++ SwiftUIMastering/SwiftUIMasteringApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMasteringApp.swift
+//  SwiftUIMastering
+//
+//  Created by VF on 18.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMasteringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
